c0cf6331a5337c69d353b461e6f2c34639fe8786
Michał B <cyberwolf_96@outlook.com>
2018-12-29 18:47:25 +0100

    Fully working app

---
+++ GoldenThoughts/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { ThoughtsService } from './thoughts.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public text: String = 'First golden thought';
+  public thoughts: Array<String> = []
+
+  constructor(
+    private thoughtsService: ThoughtsService
+  ) { }
+
+  private ngOnInit() {
+    this.thoughts = this.thoughtsService.fetchThoughts()
+  }
+
+  public addThought(): void {
+    this.thoughts.push(this.text)
+    this.text = ""
+    this.thoughtsService.saveThoughts(this.thoughts)
+  }
+
+  public removeThought(i): void {
+    this.thoughts.splice(i, 1)
+    this.thoughtsService.saveThoughts(this.thoughts)
+  }
+}
